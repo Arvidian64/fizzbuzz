16a196faad5ea1f48329f246fe3b19e77a5a5612
Arvidian64 <[arvid.sthl@yahoo.com]>
2024-08-15 11:21:43 +0200

    Had the FizzBuzz function add the value to my paragraph

--- script.js
+++ script.js
@@ -5,18 +5,23 @@ console.log(htmlbutton)
 function FizzBuzz(number) {
     console.log("FizzBuzz is running");
     let answer = number;
+    let answerSpot = document.querySelector("#answer");
     for (let i = 1; i <= answer; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
-            console.log("FizzBuzz")
+            console.log("FizzBuzz");
+            answerSpot.innerHTML += "FizzBuzz<br>";
         }
         else if (i % 3 === 0) {
             console.log("Fizz")
+            answerSpot.innerHTML += "Fizz<br>";
         }
         else if (i % 5 === 0) {
             console.log("Buzz")
+            answerSpot.innerHTML += "Buzz<br>";
         }
         else {
             console.log(i);
+            answerSpot.innerHTML += i + ("<br>");
         }
     }
 }
